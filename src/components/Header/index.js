db5372554bb63abd96081383a60307308477376f
Michael Cervantes <mac95860@hotmail.com>
2021-05-16 14:20:52 -0500

    test

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Header.css"
+
+export default function index() {
+    return (
+        <header className = "header">
+            Michael Cervantes
+        </header>
+    )
+}
